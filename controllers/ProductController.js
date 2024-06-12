32525f780503c6e62f9c41463235dfa37d36ee98
AdieJs <adieid21@gmail.com>
2024-06-12 16:00:09 +0700

    create endpoint api

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -25,14 +25,51 @@ export const getProductById = async (req, res) => {
 
 }
 
-export const createProduct = (req, res) => {
+export const createProduct = async (req, res) => {
+    const {name, price} = req.body;
+    try {
+        const product = await prisma.product.create({
+            data: {
+                name: name,
+                price: price
+            }
+        });
+        res.status(201).json(product);
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
 
 }
 
-export const updateProduct = (req, res) => {
+export const updateProduct = async (req, res) => {
+    const {name, price} = req.body;
+    try {
+        const product = await prisma.product.update({
+            where: {
+                id: Number(req.params.id)
+            },
+            data: {
+                name: name,
+                price: price
+            }
+        });
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
 
 }
 
-export const deleteProduct = (req, res) => {
+export const deleteProduct = async (req, res) => {
+    try {
+        const product = await prisma.product.delete({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
 
 }
